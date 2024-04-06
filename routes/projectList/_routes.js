3579514c980a7432b53ec8ad2f9b915df66bb925
lakshya404stc <lakshyag404stc.com>
2024-04-06 14:27:27 +0530

    projectList

--- routes/projectList/_routes.js
+++ routes/projectList/_routes.js
@@ -7,5 +7,17 @@ router.get('/get-all-projectlist', projectList.getAllProjectList);
 router.get('/get-single-projectlist', projectList.getSingleProject);
 router.delete('/delete-projectlist', projectList.deleteProjectList);
 router.put('/update-projectlist', projectList.updateProjectList);
+router.post('/create-buildinglist',projectList.createBuilding);
+router.post('/update-buildinglist',projectList.updateBuilding);
+router.post('/create-floorlist',projectList.createFloorList);
+router.post('/update-floorlist',projectList.updateFloorList);
+router.post('/create-flatlist',projectList.createFlat);
+router.post('/update-flatlist',projectList.updateFlat);
+router.post('/create-plotlist',projectList.createPlot);
+router.post('/update-plotlist',projectList.updatePlot);
+router.post('/create-rowHouselist',projectList.createRowHouse);
+router.post('/update-rowHouseList',projectList.updateRowHouse);
+router.post('/create-farmLandList',projectList.createFarmLand);
+router.post('/update-farmLandList',projectList.updateFarmLand)
 
 module.exports = router
--- routes/projectList/projectList.js
+++ routes/projectList/projectList.js
@@ -8,12 +8,10 @@ const projectList = {
       const newProject = new projectListModel(projectList);
 
       const savedProject = await newProject.save();
-      res
-        .status(201)
-        .json({
-          message: "Project created successfully",
-          project: savedProject,
-        });
+      res.status(201).json({
+        message: "Project created successfully",
+        project: savedProject,
+      });
     } catch (error) {
       res
         .status(500)
@@ -85,21 +83,354 @@ const projectList = {
   },
   updateProjectList: async (req, res) => {
     try {
-        const projectId = req.query.id;
-        const { updatedProject } = req.body;
+      const projectId = req.query.id;
+      const { updatedProject } = req.body;
 
-        const updateProjectList = await projectListModel.findByIdAndUpdate(projectId, updatedProject, { new: true });
+      const updateProjectList = await projectListModel.findByIdAndUpdate(
+        projectId,
+        updatedProject,
+        { new: true }
+      );
 
-        if (updateProjectList) {
-            return res.status(200).send({ message: "Successfully updated", updateProjectList });
-        } else {
-            return res.status(404).send({ message: "Project not found" });
-        }
+      if (updateProjectList) {
+        return res
+          .status(200)
+          .send({ message: "Successfully updated", updateProjectList });
+      } else {
+        return res.status(404).send({ message: "Project not found" });
+      }
     } catch (error) {
-        console.error("Error:", error);
-        return res.status(500).send({ message: "Something went wrong", error });
+      console.error("Error:", error);
+      return res.status(500).send({ message: "Something went wrong", error });
     }
-},
-};
+  },
+
+  createBuilding: async (req, res) => {
+    try {
+      const projectId = req.body.projectId;
+      const buildingData = req.body.buildingData;
+
+      const project = await projectListModel.findById(projectId);
+
+      if (!project) {
+        return res.status(404).json({ message: "Project not found" });
+      }
+
+      project.buildingList.push(buildingData);
+      await project.save();
+
+      res.status(201).json({ message: "Building created successfully" });
+    } catch (error) {
+      res.status(500).json({ error: "Failed to create building", details: error.message });
+    }
+  },
+
+  updateBuilding: async (req, res) => {
+    try {
+      const projectId = req.body.projectId;
+      const buildingName = req.body.buildingName;
+      const updatedBuildingData = req.body.updatedBuildingData;
+
+      const project = await projectListModel.findById(projectId);
+
+      if (!project) {
+        return res.status(404).json({ message: "Project not found" });
+      }
+
+      const building = project.buildingList.find(b => b.buildingName === buildingName);
+
+      if (!building) {
+        return res.status(404).json({ message: "Building not found within the project" });
+      }
+
+      Object.assign(building, updatedBuildingData);
+      await project.save();
+
+      res.status(200).json({ message: "Building updated successfully", updatedBuilding: building });
+    } catch (error) {
+      res.status(500).json({ error: "Failed to update building", details: error.message });
+    }
+  },
+
+  createFloorList: async (req, res) => {
+    try {
+      const projectId = req.body.projectId;
+      const buildingName = req.body.buildingName;
+      const floorData = req.body.floorData;
+
+      const project = await projectListModel.findById(projectId);
+
+      if (!project) {
+        return res.status(404).json({ message: "Project not found" });
+      }
+      const building = project.buildingList.find(
+        (b) => b.buildingName === buildingName
+      );
+
+      if (!building) {
+        return res
+          .status(404)
+          .json({ message: "Building not found within the project" });
+      }
+      building.floorList.push(floorData);
+      await project.save();
+
+      res.status(201).json({ message: "Floor list created successfully" });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "Failed to create floor list", details: error.message });
+    }
+  },
+  updateFloorList:  async (req, res) => {
+    try {
+      const projectId = req.body.projectId;
+      const buildingName = req.body.buildingName;
+      const floorName = req.body.floorName;
+      const updatedFloorData = req.body.updatedFloorData;
+  
+      const project = await projectListModel.findById(projectId);
+  
+      if (!project) {
+        return res.status(404).json({ message: "Project not found" });
+      }
+  
+      const building = project.buildingList.find(b => b.buildingName === buildingName);
+  
+      if (!building) {
+        return res.status(404).json({ message: "Building not found within the project" });
+      }
+  
+      const floor = building.floorList.find(f => f.floorName === floorName);
+  
+      if (!floor) {
+        return res.status(404).json({ message: "Floor not found within the building" });
+      }
+  
+      Object.assign(floor, updatedFloorData);
+  
+      await project.save();
+  
+      res.status(200).json({ message: "Floor updated successfully", updatedFloor: floor });
+    } catch (error) {
+      res.status(500).json({ error: "Failed to update floor", details: error.message });
+    }
+  },
+  createFlat: async (req, res) => {
+    try {
+      const projectId = req.body.projectId;
+      const buildingName = req.body.buildingName;
+      const floorName = req.body.floorName;
+      const flatData = req.body.flatData;
+
+      const project = await projectListModel.findById(projectId);
+
+      if (!project) {
+        return res.status(404).json({ message: "Project not found" });
+      }
+
+      const building = project.buildingList.find(b => b.buildingName === buildingName);
+
+      if (!building) {
+        return res.status(404).json({ message: "Building not found within the project" });
+      }
+
+      const floor = building.floorList.find(f => f.floorName === floorName);
+
+      if (!floor) {
+        return res.status(404).json({ message: "Floor not found within the building" });
+      }
+      floor.flatList.push(flatData);
+
+      await project.save();
+
+      res.status(201).json({ message: "Flat added successfully" });
+    } catch (error) {
+      res.status(500).json({ error: "Failed to add flat", details: error.message });
+    }
+  },
+
+  updateFlat: async (req, res) => {
+    try {
+      const projectId = req.body.projectId;
+      const buildingName = req.body.buildingName;
+      const floorName = req.body.floorName;
+      const flatName = req.body.flatName;
+      const updatedFlatData = req.body.updatedFlatData;
+
+      const project = await projectListModel.findById(projectId);
+
+      if (!project) {
+        return res.status(404).json({ message: "Project not found" });
+      }
+
+      const building = project.buildingList.find(b => b.buildingName === buildingName);
+
+      if (!building) {
+        return res.status(404).json({ message: "Building not found within the project" });
+      }
+
+      const floor = building.floorList.find(f => f.floorName === floorName);
+
+      if (!floor) {
+        return res.status(404).json({ message: "Floor not found within the building" });
+      }
+
+      const flat = floor.flatList.find(f => f.flatName === parseInt(flatName));
+
+      if (!flat) {
+        return res.status(404).json({ message: "Flat not found within the floor" });
+      }
+
+      Object.assign(flat, updatedFlatData);
+
+      await project.save();
+
+      res.status(200).json({ message: "Flat updated successfully", updatedFlat: flat });
+    } catch (error) {
+      res.status(500).json({ error: "Failed to update flat", details: error.message });
+    }
+  },
+  createPlot: async (req, res) => {
+    try {
+      const projectId = req.body.projectId;
+      const plotData = req.body.plotData;
+
+      const project = await projectListModel.findById(projectId);
+
+      if (!project) {
+        return res.status(404).json({ message: "Project not found" });
+      }
+
+      project.plotList.push(plotData);
+      await project.save();
+
+      res.status(201).json({ message: "Building created successfully" });
+    } catch (error) {
+      res.status(500).json({ error: "Failed to create building", details: error.message });
+    }
+  },
+
+  updatePlot: async (req, res) => {
+    try {
+      const projectId = req.body.projectId;
+      const plotName = req.body.plotName;
+      const updatedPlotData = req.body.updatedPlotData;
+
+      const project = await projectListModel.findById(projectId);
+
+      if (!project) {
+        return res.status(404).json({ message: "Project not found" });
+      }
+
+      const plot = project.plotList.find(b => b.plotName === plotName);
+
+      if (!plot) {
+        return res.status(404).json({ message: "plot not found within the project" });
+      }
+
+      Object.assign(plot, updatedPlotData);
+      await project.save();
+
+      res.status(200).json({ message: "plot updated successfully", updatedplot: plot });
+    } catch (error) {
+      res.status(500).json({ error: "Failed to update plot", details: error.message });
+    }
+  },
+  createRowHouse: async (req, res) => {
+    try {
+      const projectId = req.body.projectId;
+      const rowHouseData = req.body.rowHouseData;
+
+      // Find the project by ID
+      const project = await projectListModel.findById(projectId);
+
+      if (!project) {
+        return res.status(404).json({ message: "Project not found" });
+      }
+
+      // Add the new row house to the row house list
+      project.rowHouseList.push(rowHouseData);
+
+      // Save the updated project
+      await project.save();
+
+      res.status(201).json({ message: "Row house created successfully" });
+    } catch (error) {
+      res.status(500).json({ error: "Failed to create row house", details: error.message });
+    }
+  },
+
+  updateRowHouse: async (req, res) => {
+    try {
+      const projectId = req.body.projectId;
+      const rowHouseName = req.body.rowHouseName;
+      const updatedRowHouseData = req.body.updatedRowHouseData;
+
+      const project = await projectListModel.findById(projectId);
+
+      if (!project) {
+        return res.status(404).json({ message: "Project not found" });
+      }
+      const rowHouse = project.rowHouseList.find(rh => rh.rowHouseName === rowHouseName);
+
+      if (!rowHouse) {
+        return res.status(404).json({ message: "Row house not found within the project" });
+      }
+      Object.assign(rowHouse, updatedRowHouseData);
+      await project.save();
+
+      res.status(200).json({ message: "Row house updated successfully", updatedRowHouse: rowHouse });
+    } catch (error) {
+      res.status(500).json({ error: "Failed to update row house", details: error.message });
+    }
+  },
+  createFarmLand: async (req, res) => {
+    try {
+      const projectId = req.body.projectId;
+      const farmLandData = req.body.farmLandData;
+
+      const project = await projectListModel.findById(projectId);
+
+      if (!project) {
+        return res.status(404).json({ message: "Project not found" });
+      }
+
+      project.farmLandList.push(farmLandData);
+      await project.save();
+
+      res.status(201).json({ message: "Farm land created successfully" });
+    } catch (error) {
+      res.status(500).json({ error: "Failed to create farm land", details: error.message });
+    }
+  },
+
+  updateFarmLand: async (req, res) => {
+    try {
+      const projectId = req.body.projectId;
+      const farmLandName = req.body.farmLandName;
+      const updatedFarmLandData = req.body.updatedFarmLandData;
+
+      const project = await projectListModel.findById(projectId);
+
+      if (!project) {
+        return res.status(404).json({ message: "Project not found" });
+      }
+
+      const farmLand = project.farmLandList.find(fl => fl.farmLandName === farmLandName);
+
+      if (!farmLand) {
+        return res.status(404).json({ message: "Farm land not found within the project" });
+      }
+
+      Object.assign(farmLand, updatedFarmLandData);
+      await project.save();
+
+      res.status(200).json({ message: "Farm land updated successfully", updatedFarmLand: farmLand });
+    } catch (error) {
+      res.status(500).json({ error: "Failed to update farm land", details: error.message });
+    }
+  }
+}
 
 module.exports = projectList;
